f1ac20fffda6de1f19c197cf43b961a8bcf178fa
092vk <vk092kumar@gmail.com>
2025-11-12 13:13:39 +0530

    added boot.asm and commands

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Boot_loader_for_32bit_Kernel
 
+## Tools required: Install the following tools
+bash`
+sudo apt install nasm
+sudo apt install qemu-system-x86
+sudo apt install bless
+`
+
--- src/boot.asm
+++ src/boot.asm
@@ -1,1 +1,21 @@
-;this is a comment
+[BITS 16]           ;Set the mode to 16bit
+[ORG 0x7c00]        ;Set the origin address to 0x7c00 , typical for boot loaders
+
+
+start:
+    cli             ;Clear interrupts, disabling all maskable interrupts            
+    mov ax, 0x00    ;Load immediate value 0x00 into register AX(accumulator register)
+    mov ds, ax      ;Set data segment register to 0x00
+    mov es, ax      ;Set extraa segment register to 0x00
+    mov ss, ax      ;Set stack segment to 0x00
+    mov sp, 0x7c00  ;Set stack pointer to 0x7c00 , top of the bootloader segment
+    mov si, msg     ;load the address of msg into source index SI register
+    sti             ;Enable the interrupts which were disabled
+
+print:
+    lodsb           ;Load the byte as DS:SI into AL register and increment SI
+
+done:
+    cli
+    hlt
+
